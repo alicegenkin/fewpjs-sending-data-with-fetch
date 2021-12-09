fb0f0def3f9aaddff48ff4e3ab86f10d712b2cfa
Alice Genkin <agenkin1@gmail.com>
2021-12-09 12:48:33 -0500

    commit tree 6bb8772cb17533a1a051547f7dfb19dff6f216c6

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  //post request to destination url
+  //headers have content type and accept
+  //body with name and email stringified
+return fetch('http://localhost:3000/users', {
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( {
+      name,
+      email
+    })
+  })
+    //use then to access res obj and built in json method to parse contents of body properly
+    .then (function(response) {
+      return response.json()
+    })
+    .then(function(object) {
+      document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message
+    })
+  }
